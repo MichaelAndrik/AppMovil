e49049c667e1d31db0dc9b0a2b0dcda55e6ef4d1
MichaelAndrik <michaelandrik10@gmail.com>
2022-01-07 11:53:34 -0500

    refactor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
+  firebase: {
+    projectId: 'ionicchat-eccd8',
+    appId: '1:363994803067:web:3e7aaa19f00502370a6bde',
+    storageBucket: 'ionicchat-eccd8.appspot.com',
+    apiKey: 'AIzaSyClMoQC1LuzDzE3gdlmMjI_JhviiOTmm-Y',
+    authDomain: 'ionicchat-eccd8.firebaseapp.com',
+    messagingSenderId: '363994803067',
+  },
   production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ionicchat-eccd8',
+    appId: '1:363994803067:web:3e7aaa19f00502370a6bde',
+    storageBucket: 'ionicchat-eccd8.appspot.com',
+    apiKey: 'AIzaSyClMoQC1LuzDzE3gdlmMjI_JhviiOTmm-Y',
+    authDomain: 'ionicchat-eccd8.firebaseapp.com',
+    messagingSenderId: '363994803067',
+  },
   production: false
 };
 
